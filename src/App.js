c74db1d16253aeb58ec8db2600a36fe94d2d1b0e
KlimovI <irracli@gmail.com>
2020-02-12 20:49:27 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import List from './Components/List/List'
 
 function App() {
+  const items = [1,2,3,4,5,6,7,8,9,10,11];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <h1>React</h1>
+
+      <List items = {items} />
     </div>
   );
 }
